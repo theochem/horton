2bedabfcb41bb5a65e6cde6e328f739236310ff5
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2012-04-06 15:19:59 +0200

    Fix error in updatesite.sh script

--- updatesite.sh
+++ updatesite.sh
@@ -14,10 +14,9 @@ git checkout master && (
     git rm $(git ls-tree $(git log --color=never | head -n1 | cut -f2 -d' ') -r --name-only)
     cp -rv doc/_build/html/* .
     for f in $(find doc/_build/html | cut -c17-); do
-
       git add $f
     done
     git commit -a -m 'Automatic documentation update'
-    git push official gh-pages:gh-pages
+    git push origin gh-pages:gh-pages
   )
 ) && git checkout master
