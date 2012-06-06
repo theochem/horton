afb737775ab82f89b3142628ed1e643f3a9b1983
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2012-06-06 17:54:46 +0200

    New update site script

--- updatesite.sh
+++ updatesite.sh
@@ -6,9 +6,13 @@ git checkout master && (
     make html
     cd _build/html
     mv _static static
-    for f in $(find); do sed -e 's/_static/static/' -i $f; done
+    for f in $(find); do sed -e 's/_static/static/g' -i $f; done
     mv _sources sources
-    for f in $(find); do sed -e 's/_sources/sources/' -i $f; done
+    for f in $(find); do sed -e 's/_sources/sources/g' -i $f; done
+    mv _images images
+    for f in $(find); do sed -e 's/_images/images/g' -i $f; done
+    mv _downloads downloads
+    for f in $(find); do sed -e 's/_downloads/downloads/g' -i $f; done
   )
   git checkout gh-pages && (
     git rm $(git ls-tree $(git log --color=never | head -n1 | cut -f2 -d' ') -r --name-only)
