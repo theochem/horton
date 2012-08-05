a0f6a63b1d7dbaa93ef5c01948380ec473a59c27
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2012-08-05 08:06:03 +0200

    Operator checkpointing

--- horton/test/test_checkpoint.py
+++ horton/test/test_checkpoint.py
@@ -33,9 +33,40 @@ def compare_expansions(e1, e2):
         assert (e1.energies == e2.energies).all()
 
 
+def compare_one_body(sys1, sys2, key):
+    if key in sys1.operators:
+        assert key in sys2.operators
+        op1 = sys1.operators[key]
+        op2 = sys2.operators[key]
+        if isinstance(op1, DenseOneBody):
+            assert isinstance(op2, DenseOneBody)
+            assert op1.nbasis == op2.nbasis
+            assert (op1._array == op2._array).all()
+        else:
+            raise NotImplementedError
+    else:
+        assert key not in sys2.operators
+
+
+def compare_two_body(sys1, sys2, key):
+    if key in sys1.operators:
+        assert key in sys2.operators
+        op1 = sys1.operators[key]
+        op2 = sys2.operators[key]
+        if isinstance(op1, DenseTwoBody):
+            assert isinstance(op2, DenseTwoBody)
+            assert op1.nbasis == op2.nbasis
+            assert (op1._array == op2._array).all()
+        else:
+            raise NotImplementedError
+    else:
+        assert key not in sys2.operators
+
+
 def compare_systems(sys1, sys2):
     assert (sys1.numbers == sys2.numbers).all()
     assert (sys1.coordinates == sys2.coordinates).all()
+    # basis
     if sys1.basis is not None:
         assert (sys1.basis.centers == sys2.basis.centers).all()
         assert (sys1.basis.shell_map == sys2.basis.shell_map).all()
@@ -45,6 +76,7 @@ def compare_systems(sys1, sys2):
         assert (sys1.basis.con_coeffs == sys2.basis.con_coeffs).all()
     else:
         assert sys2.basis is None
+    # wfn
     if isinstance(sys1.wfn, ClosedShellWFN):
         assert isinstance(sys2.wfn, ClosedShellWFN)
         assert sys1.wfn.nep == sys2.wfn.nep
@@ -64,15 +96,22 @@ def compare_systems(sys1, sys2):
         assert sys2.wfn is None
     else:
         raise NotImplementedError
-
+    # one-body operators
+    compare_one_body(sys1, sys2, 'olp')
+    compare_one_body(sys1, sys2, 'kin')
+    compare_one_body(sys1, sys2, 'na')
+    # two-body operators
+    compare_two_body(sys1, sys2, 'er')
 
 def test_chk_initialization_filename_cs():
     tmpdir = tempfile.mkdtemp('horton.test.test_checkpoint.test_chk_initialization_filename_cs')
     try:
         fn_chk = '%s/chk.h5' % tmpdir
-        sys1 = System.from_file(context.get_fn('test/hf_sto3g.fchk'), chk=fn_chk)
+        fn_fchk = context.get_fn('test/water_sto3g_hf_g03.fchk')
+        fn_log = context.get_fn('test/water_sto3g_hf_g03.log')
+        sys1 = System.from_file(fn_fchk, fn_log, chk=fn_chk)
         del sys1
-        sys1 = System.from_file(context.get_fn('test/hf_sto3g.fchk'))
+        sys1 = System.from_file(fn_fchk, fn_log)
         sys2 = System.from_file(fn_chk)
         assert sys2.wfn is not None
         compare_systems(sys1, sys2)
