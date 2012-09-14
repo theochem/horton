a1abeecf576d37c06fa6b6a271e7d92851099e1b
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2012-09-14 18:38:58 +0200

    Updated example

--- examples/001_hf_water/run.py
+++ examples/001_hf_water/run.py
@@ -16,8 +16,15 @@ ham = Hamiltonian(sys, [HartreeFock()])
 
 print 'SCF cycle'
 converged = converge_scf(ham)
-print 'Converged?', converged
+print 'Converged:', converged
 
 print 'Computing energies'
 ham.compute_energy()
 print sys.props
+
+print 'Compute Becke charges'
+rtf = LogRTransform(TrapezoidIntegrator1D(), 1e-3, 0.1)
+grid = BeckeMolGrid(sys, (rtf, 110, 100), random_rotate=False, keep_subgrids=1)
+bdp = BeckeDPart(grid)
+bdp.do_charges()
+print bdp['charges']
