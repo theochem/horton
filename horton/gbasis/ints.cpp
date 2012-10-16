f350ce6d5e42d571dd2cea906663873249aa889a
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2012-10-16 06:08:22 +0200

    Fix compilation on sharcnet

--- horton/gbasis/ints.cpp
+++ horton/gbasis/ints.cpp
@@ -22,11 +22,12 @@
 #include <cmath>
 #include <cstring>
 #include <stdexcept>
+#include <cstdlib>
 #include "boys.h"
 #include "cartpure.h"
 #include "common.h"
 #include "ints.h"
-using namespace std;
+using std::abs;
 
 /*
 
@@ -99,10 +100,10 @@ GB2Integral::GB2Integral(long max_shell_type): GBCalculator(max_shell_type) {
 
 void GB2Integral::reset(long _shell_type0, long _shell_type1, const double* _r0, const double* _r1) {
     if ((_shell_type0 < -max_shell_type) || (_shell_type0 > max_shell_type)) {
-      throw domain_error("shell_type0 out of range.");
+      throw std::domain_error("shell_type0 out of range.");
     }
     if ((_shell_type1 < -max_shell_type) || (_shell_type1 > max_shell_type)) {
-      throw domain_error("shell_type1 out of range.");
+      throw std::domain_error("shell_type1 out of range.");
     }
     shell_type0 = _shell_type0;
     shell_type1 = _shell_type1;
@@ -338,16 +339,16 @@ GB4Integral::GB4Integral(long max_shell_type): GBCalculator(max_shell_type) {
 void GB4Integral::reset(long _shell_type0, long _shell_type1, long _shell_type2, long _shell_type3,
                         const double* _r0, const double* _r1, const double* _r2, const double* _r3) {
     if ((_shell_type0 < -max_shell_type) || (_shell_type0 > max_shell_type)) {
-      throw domain_error("shell_type0 out of range.");
+      throw std::domain_error("shell_type0 out of range.");
     }
     if ((_shell_type1 < -max_shell_type) || (_shell_type1 > max_shell_type)) {
-      throw domain_error("shell_type1 out of range.");
+      throw std::domain_error("shell_type1 out of range.");
     }
     if ((_shell_type2 < -max_shell_type) || (_shell_type2 > max_shell_type)) {
-      throw domain_error("shell_type2 out of range.");
+      throw std::domain_error("shell_type2 out of range.");
     }
     if ((_shell_type3 < -max_shell_type) || (_shell_type3 > max_shell_type)) {
-      throw domain_error("shell_type3 out of range.");
+      throw std::domain_error("shell_type3 out of range.");
     }
     shell_type0 = _shell_type0;
     shell_type1 = _shell_type1;
