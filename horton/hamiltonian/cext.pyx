b7de6abd172a1d8ebea92e7d0e082c064b2c27dc
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2012-10-19 18:05:41 +0200

    Spin polarized LDA functionals from libxc

--- horton/hamiltonian/cext.pyx
+++ horton/hamiltonian/cext.pyx
@@ -27,6 +27,7 @@ np.import_array()
 
 cdef extern from "xc.h":
     enum: XC_UNPOLARIZED
+    enum: XC_POLARIZED
 
     ctypedef struct xc_func_type:
         pass
@@ -40,7 +41,8 @@ cdef extern from "xc.h":
 
 
 cdef class LibXCWrapper(object):
-    cdef xc_func_type _func
+    cdef xc_func_type _func_pol
+    cdef xc_func_type _func_unpol
     cdef int _func_id
 
     def __cinit__(self, bytes name):
@@ -54,24 +56,45 @@ cdef class LibXCWrapper(object):
         self._func_id = xc_functional_get_number(name)
         if self._func_id < 0:
             raise ValueError('Unknown LibXC functional name: %s' % name)
-        retcode = xc_func_init(&self._func, self._func_id, XC_UNPOLARIZED)
+        retcode = xc_func_init(&self._func_pol, self._func_id, XC_POLARIZED)
         if retcode != 0:
-            raise ValueError('Could not initialize LibXC functional: %s' % name)
+            raise ValueError('Could not initialize polarized LibXC functional: %s' % name)
+        retcode = xc_func_init(&self._func_unpol, self._func_id, XC_UNPOLARIZED)
+        if retcode != 0:
+            raise ValueError('Could not initialize unpolarized LibXC functional: %s' % name)
 
     def __dealloc__(self):
         if self._func_id >= 0:
-            xc_lda_end(&self._func)
+            xc_lda_end(&self._func_pol)
+            xc_lda_end(&self._func_unpol)
 
-    def compute_exc(self, np.ndarray[double, ndim=1] rho, np.ndarray[double, ndim=1] zk):
+    def compute_exc_unpol(self, np.ndarray[double, ndim=1] rho, np.ndarray[double, ndim=1] zk):
         assert rho.flags['C_CONTIGUOUS']
         npoint = rho.shape[0]
         assert zk.flags['C_CONTIGUOUS']
         assert npoint == zk.shape[0]
-        xc_lda_exc(&self._func, npoint, <double*>rho.data, <double*>zk.data)
+        xc_lda_exc(&self._func_unpol, npoint, <double*>rho.data, <double*>zk.data)
 
-    def compute_vxc(self, np.ndarray[double, ndim=1] rho, np.ndarray[double, ndim=1] vxc):
+    def compute_exc_pol(self, np.ndarray[double, ndim=2] rho, np.ndarray[double, ndim=1] zk):
+        assert rho.flags['C_CONTIGUOUS']
+        npoint = rho.shape[0]
+        assert rho.shape[1] == 2
+        assert zk.flags['C_CONTIGUOUS']
+        assert npoint == zk.shape[0]
+        xc_lda_exc(&self._func_pol, npoint, <double*>rho.data, <double*>zk.data)
+
+    def compute_vxc_unpol(self, np.ndarray[double, ndim=1] rho, np.ndarray[double, ndim=1] vxc):
         assert rho.flags['C_CONTIGUOUS']
         npoint = rho.shape[0]
         assert vxc.flags['C_CONTIGUOUS']
         assert npoint == vxc.shape[0]
-        xc_lda_vxc(&self._func, npoint, <double*>rho.data, <double*>vxc.data)
+        xc_lda_vxc(&self._func_unpol, npoint, <double*>rho.data, <double*>vxc.data)
+
+    def compute_vxc_pol(self, np.ndarray[double, ndim=2] rho, np.ndarray[double, ndim=2] vxc):
+        assert rho.flags['C_CONTIGUOUS']
+        npoint = rho.shape[0]
+        assert rho.shape[1] == 2
+        assert vxc.flags['C_CONTIGUOUS']
+        assert npoint == vxc.shape[0]
+        assert vxc.shape[1] == 2
+        xc_lda_vxc(&self._func_pol, npoint, <double*>rho.data, <double*>vxc.data)
--- horton/hamiltonian/libxc.py
+++ horton/hamiltonian/libxc.py
@@ -20,6 +20,8 @@
 #--
 
 
+import numpy as np
+
 from horton.hamiltonian.core import HamiltonianTerm
 from horton.hamiltonian.cext import LibXCWrapper
 
@@ -42,34 +44,77 @@ class LibXCTerm(HamiltonianTerm):
         self._libxc_wrapper = LibXCWrapper(name)
 
     def _update_operator(self):
-        assert self.system.wfn.closed_shell
+        if self.system.wfn.closed_shell:
+            # In the closed-shell case, libxc expects the total density as input
+            # and returns the potential for the alpha electrons.
+            pot, new = self.cache.load('pot_libxc_%s_alpha' % self._name, alloc=self.grid.size)
+            if new:
+                rho = self.update_rho('full')
+                self._libxc_wrapper.compute_vxc_unpol(rho, pot)
 
-        # In the closed-shell case, libxc expects the total density as input
-        # and returns the potential for the alpha electrons.
-        rho = self.update_rho('full')
-        pot, new = self.cache.load('pot_libxc_%s_%s' % (self._name, 'alpha'), alloc=self.grid.size)
-        if new:
-            self._libxc_wrapper.compute_vxc(rho, pot)
+            # TODO: in the Hamiltonian class, all the grids should be added
+            # and converted only once to a one-body operator
+            # Create/update the one-body operator based on the potential on the grid.
+            operator, new = self.cache.load('op_libxc_%s_alpha' % self._name, alloc=(self.system.lf, 'one_body'))
+            if new:
+                self.system.compute_grid_one_body(self.grid.points, self.grid.weights, pot, operator)
+        else:
+            # In case of spin-polarized computations, alpha and beta densities
+            # go in and the alpha and beta potentials come out of  it.
+            pot_alpha, new_alpha = self.cache.load('pot_libxc_%s_alpha' % self._name, alloc=self.grid.size)
+            pot_beta, new_beta = self.cache.load('pot_libxc_%s_beta' % self._name, alloc=self.grid.size)
+            if new_alpha or new_beta:
+                # TODO: rho_both has to be cashed
+                rho_both = np.zeros((len(pot_alpha), 2), float)
+                rho_both[:,0] = self.update_rho('alpha')
+                rho_both[:,1] = self.update_rho('beta')
+                # TODO: pot_both has to be cashed
+                pot_both = np.zeros((len(pot_alpha), 2), float)
+                self._libxc_wrapper.compute_vxc_pol(rho_both, pot_both)
+                pot_alpha[:] = pot_both[:,0]
+                pot_beta[:] = pot_both[:,1]
 
-        # Create/update the one-body operator based on the potential on the grid.
-        operator, new = self.cache.load('op_libxc_%s_%s' % (self._name, 'alpha'), alloc=(self.system.lf, 'one_body'))
-        if new:
-            self.system.compute_grid_one_body(self.grid.points, self.grid.weights, pot, operator)
+            # TODO: in the Hamiltonian class, all the grids should be added
+            # and converted only once to a one-body operator
+            # Create/update the one-body operator based on the potential on the grid.
+            operator_alpha, new_alpha = self.cache.load('op_libxc_%s_alpha' % self._name, alloc=(self.system.lf, 'one_body'))
+            if new_alpha:
+                self.system.compute_grid_one_body(self.grid.points, self.grid.weights, pot_alpha, operator_alpha)
+
+            operator_beta, new_beta = self.cache.load('op_libxc_%s_beta' % self._name, alloc=(self.system.lf, 'one_body'))
+            if new_beta:
+                self.system.compute_grid_one_body(self.grid.points, self.grid.weights, pot_beta, operator_beta)
 
     def compute_energy(self):
-        assert self.system.wfn.closed_shell
+        if self.system.wfn.closed_shell:
+            # In the unpolarized case, libxc expects the total density as input
+            # and returns the energy density per particle.
+            rho = self.update_rho('full')
+            edens, new = self.cache.load('edens_libxc_%s_full' % self._name, alloc=self.grid.size)
+            if new:
+                self._libxc_wrapper.compute_exc_unpol(rho, edens)
+            energy = self.grid.integrate(edens, rho)
+            self.store_energy('libxc_%s' % self._name, energy)
+            return energy
+        else:
+            # In case of spin-polarized computations, alpha and beta densities
+            # go in and the 'total' energy density comes out.
+            rho_alpha = self.update_rho('alpha')
+            rho_beta = self.update_rho('beta')
+            edens, new = self.cache.load('edens_libxc_%s_full' % self._name, alloc=self.grid.size)
+            if new:
+                # TODO: rho_both has to be cashed
+                rho_both = np.zeros((len(rho_alpha), 2), float)
+                rho_both[:,0] = rho_alpha
+                rho_both[:,1] = rho_beta
+                self._libxc_wrapper.compute_exc_pol(rho_both, edens)
 
-        # In the closed-shell case, libxc expects the total density as input
-        # and returns the energy density per particle.
-        rho = self.update_rho('full')
-        edens, new = self.cache.load('edens_libxc_%s_%s' % (self._name, 'full'), alloc=self.grid.size)
-        if new:
-            self._libxc_wrapper.compute_exc(rho, edens)
-        energy = self.grid.integrate(edens, rho)
-        self.store_energy('libxc_%s' % self._name, energy)
-        return energy
+            energy = self.grid.integrate(edens, rho_alpha) + self.grid.integrate(edens, rho_beta)
+            self.store_energy('libxc_%s' % self._name, energy)
+            return energy
 
     def add_fock_matrix(self, fock_alpha, fock_beta):
-        assert self.system.wfn.closed_shell
         self._update_operator()
-        fock_alpha.iadd(self.cache.load('op_libxc_%s_%s' % (self._name, 'alpha')))
+        fock_alpha.iadd(self.cache.load('op_libxc_%s_alpha' % self._name))
+        if not self.system.wfn.closed_shell:
+            fock_beta.iadd(self.cache.load('op_libxc_%s_beta' % self._name, ))
