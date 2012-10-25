bfafdfb2a1d66b1b8a6670c7fb84eb0b6834c759
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2012-10-25 06:20:42 +0200

    Added missing files

---
+++ horton/nucpot.cpp
@@ -0,0 +1,43 @@
+// Horton is a Density Functional Theory program.
+// Copyright (C) 2011-2012 Toon Verstraelen <Toon.Verstraelen@UGent.be>
+//
+// This file is part of Horton.
+//
+// Horton is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 3
+// of the License, or (at your option) any later version.
+//
+// Horton is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>
+//
+//--
+
+
+#include <cmath>
+#include "nucpot.h"
+
+void compute_grid_nucpot(long* numbers, double* coordinates, long natom,
+                         double* points, double* output, long npoint) {
+    for (long ipoint=npoint-1; ipoint >= 0; ipoint--) {
+        double tmp = 0.0;
+        for (long iatom=natom-1; iatom >= 0; iatom--) {
+            double delta = points[0]-coordinates[3*iatom];
+            double dsq = delta*delta;
+            delta = points[1]-coordinates[3*iatom+1];
+            dsq += delta*delta;
+            delta = points[2]-coordinates[3*iatom+2];
+            dsq += delta*delta;
+            tmp += numbers[iatom]/sqrt(dsq);
+        }
+        *output += tmp;
+
+        points += 3;
+        output++;
+    }
+}
--- tools/cmp_gaussian/compare.py
+++ tools/cmp_gaussian/compare.py
@@ -3,6 +3,7 @@
 import os, numpy as np
 from glob import glob
 from horton import *
+from horton.hamiltonian.test.common import check_cubic_os_wrapper
 
 #log.set_level(log.high)
 log.set_level(log.silent)
@@ -10,16 +11,19 @@ log.set_level(log.silent)
 #fns_fchk = ['006__C_Q+0_M7/gaussian.fchk']
 #fns_fchk = ['006__C_Q+0_M5/gaussian.fchk']
 #fns_fchk = ['006__C_Q+2_M1/gaussian.fchk']
+fns_fchk = ['005__B_Q-1_M7/gaussian.fchk']
 #fns_fchk = ['010_Ne_Q+0_M1/gaussian.fchk']
 #fns_fchk = ['006__C_Q-1_M2/gaussian.fchk']
 #fns_fchk = ['013_Al_Q-1_M7/gaussian.fchk']
-fns_fchk = sorted(glob('*/gaussian.fchk'))
+#fns_fchk = sorted(glob('*/gaussian.fchk'))
 
 np.set_printoptions(suppress=True, linewidth=100)
 print '                          Case             Horton                G09        H-G  H converged'
 print '--------------------------------------------------------------------------------------------'
 for fn_fchk in fns_fchk:
     sys = System.from_file(fn_fchk)
+    dma0 = sys.wfn.dm_alpha.copy()
+    dmb0 = sys.wfn.dm_beta.copy()
     g09_energy = sys.props['energy']
     sys.props.clear()
 
@@ -28,3 +32,11 @@ for fn_fchk in fns_fchk:
     converged = converge_scf_oda(ham, max_iter=1024, threshold=1e-8, debug=False)
     error = sys.props['energy'] - g09_energy
     print '%30s  % 15.10e  % 15.10e  %+9.4f  %s' % (fn_fchk, sys.props['energy'], g09_energy, error, converged)
+
+    dma1 = sys.wfn.dm_alpha.copy()
+    dmb1 = sys.wfn.dm_beta.copy()
+    check_cubic_os_wrapper(ham, dma0, dmb0, dma1, dmb1, do_plot=True)
+    
+
+
+
