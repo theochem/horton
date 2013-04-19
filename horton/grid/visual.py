e3f734b97d35212d1220ea2a2eae308dbcc076a4
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2013-04-19 09:00:38 +0200

    Fix a few minor bugs

--- horton/grid/visual.py
+++ horton/grid/visual.py
@@ -45,7 +45,7 @@ class RectangleGrid(IntGrid):
         self.h0 = h0
         self.l1 = l1
         self.h1 = h1
-        self.shape = (h0-l0+1, h1-l1+1)
+        #self.shape = (h0-l0+1, h1-l1+1)
 
         npoint = (h0-l0+1)*(h1-l1+1)
         points = np.zeros((npoint, 3), float)
