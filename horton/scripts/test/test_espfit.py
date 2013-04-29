a621d03ebb4e2c784fb8b7b7dc900d4bbb917e2f
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2013-04-29 15:07:13 -0400

    Test for the esp fitting scripts

--- horton/scripts/test/test_espfit.py
+++ horton/scripts/test/test_espfit.py
@@ -20,7 +20,11 @@
 #--
 
 
-from horton import angstrom
+import tempfile, shutil, os, h5py as h5
+
+from horton import *
+from horton.test.common import check_script
+from horton.scripts.test.common import copy_files, check_files
 from horton.scripts.espfit import *
 
 def test_wdens():
@@ -37,3 +41,30 @@ def test_wnear():
 def test_wfar():
     assert parse_wfar('4.3') == (4.3*angstrom, 1.0*angstrom)
     assert parse_wfar('4.2:0.3') == (4.2*angstrom, 0.3*angstrom)
+
+
+def test_scripts():
+    # Generate some random system with random esp data
+    natom = 5
+    numbers = np.random.randint(1, 20, natom)
+    coordinates = np.random.uniform(0, 10, (natom, 3))
+    origin = np.zeros(3, float)
+    grid_rvecs = np.identity(3, float)*1.0
+    shape = np.array([10, 10, 10])
+    pbc = np.ones(3, int)
+    ui_grid = UniformIntGrid(origin, grid_rvecs, shape, pbc)
+    cube_data = np.random.uniform(-1, 1, shape)
+    props = {'ui_grid': ui_grid, 'cube_data': cube_data}
+    sys = System(coordinates, numbers, props=props)
+
+    # Write the cube file to the tmpdir and run scripts
+    tmpdir = tempfile.mkdtemp('horton.scripts.test.test_espfit.test_scripts')
+    try:
+        sys.to_file(os.path.join(tmpdir, 'esp.cube'))
+        check_script('horton-esp-cost.py esp.cube --wnear=0:1.0:0.5', tmpdir)
+        check_files(tmpdir, ['esp.cube.h5'])
+        check_script('horton-esp-fit.py esp.cube.h5:espfit/espfit_r1 default', tmpdir)
+        check_script('horton-esp-test.py esp.cube.h5:espfit/espfit_r1 esp.cube.h5:espfit/espfit_r1/default', tmpdir)
+        check_script('horton-esp-gen.py esp.cube.h5:espfit/espfit_r1/default --grid=1.2', tmpdir)
+    finally:
+        shutil.rmtree(tmpdir)
