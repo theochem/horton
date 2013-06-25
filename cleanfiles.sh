abde05900fef9c36e1d1fda3887dd877dacd1af6
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2013-06-25 13:16:02 +0200

    Fix typo

--- cleanfiles.sh
+++ cleanfiles.sh
@@ -9,5 +9,5 @@ rm -vr doctrees
 rm -v horton/cext.cpp
 rm -v horton/gbasis/cext.cpp
 rm -v horton/grid/cext.cpp
-rm -v horton/hamiltonian/cext.cpp
+rm -v horton/meanfield/cext.cpp
 rm -v horton/espfit/cext.cpp
