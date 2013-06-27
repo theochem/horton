b120964905beb456ed863a2ff971f1b74ff0b440
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2013-06-27 19:16:39 +0200

    Automatically generate library documentation

--- cleanfiles.sh
+++ cleanfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 for i in $(find horton tools scripts | egrep "\.pyc$|\.py~$|\.pyc~$|\.bak$|\.so$") ; do rm -v ${i}; done
 rm -vr doc/_build/
+rm -vr doc/lib
 rm -v MANIFEST
 rm -vr dist
 rm -vr build
