0dc0adabb7fc6358526976788de04c247b884626
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2013-08-02 07:35:26 +0200

    Improved ReactangleGrid and new LineGrid

--- horton/grid/test/test_visual.py
+++ horton/grid/test/test_visual.py
@@ -24,6 +24,21 @@ import numpy as np
 from horton import *
 
 
+def test_line_grid():
+    p1 = np.random.normal(0, 1, 3)
+    p2 = np.random.normal(0, 1, 3)
+    lg = LineGrid(p1, p2, 100)
+    assert abs(lg.x[0] + np.linalg.norm(p2-p1)/2) < 1e-10
+    assert abs(lg.x[-1] - np.linalg.norm(p2-p1)/2) < 1e-10
+    assert abs(lg.points[0] - p1).max() < 1e-10
+    assert abs(lg.points[-1] - p2).max() < 1e-10
+    lg = LineGrid(p1, p2, 7, extend=1)
+    assert abs(lg.x[2] + np.linalg.norm(p2-p1)/2) < 1e-10
+    assert abs(lg.x[-3] - np.linalg.norm(p2-p1)/2) < 1e-10
+    assert abs(lg.points[2] - p1).max() < 1e-10
+    assert abs(lg.points[-3] - p2).max() < 1e-10
+
+
 def test_rectangle_grid():
     center = np.array([1.0, 0.5, 0.05])
     axis0 = np.array([0.0, 0.0, 0.1])
@@ -32,10 +47,15 @@ def test_rectangle_grid():
     assert abs(g.weights - 0.01).max() < 1e-10
     assert abs(g.points.mean(axis=0) - center).max() < 1e-10
     assert abs(g.points[0] - np.array([1.0, -0.5, -0.95])).max() < 1e-10
+    foo = np.random.normal(0, 1, g.size)
+    x, y, z = g.prepare_contour(foo)
+    assert x.shape == (21,)
+    assert y.shape == (21,)
+    assert z.shape == (21, 21)
+    assert abs(z.ravel() - foo).max() == 0.0
+    assert abs(g.integrate(foo) - foo.sum()*0.01) < 1e-10
 
     axis0 = np.array([0.0, 0.0, 0.1])
     axis1 = np.array([0.0, 0.1, 0.1])
     g = RectangleGrid(center, axis0, axis1, -10, 10, -10, 10)
     assert abs(g.weights - 0.01).max() < 1e-10
-
-    # TODO test integration weights with a numerical example
--- horton/grid/visual.py
+++ horton/grid/visual.py
@@ -26,30 +26,99 @@ import numpy as np
 from horton.grid.base import IntGrid
 
 
-__all__ = ['RectangleGrid']
+__all__ = ['LineGrid', 'RectangleGrid']
+
+
+class LineGrid(IntGrid):
+    '''Grid points evenly distributed over a 3D line segment'''
+    def __init__(self, p1, p2, size, extend=0):
+        '''
+           **Arguments:**
+
+           p1, p2
+                Two points defining the line segment
+
+           size
+                The number of grid points
+
+           **Optional arguments:**
+
+           extend
+                Used to control how far the grid points extrapolate from the
+                line segment.
+        '''
+        self._p1 = p1
+        self._p2 = p2
+        norm = np.linalg.norm(p2 - p1)
+        self._x = norm*(np.arange(size, dtype=float)/(size-1)-0.5)
+        self._x *= 1 + 2*extend
+        points = np.outer(self._x, (p2 - p1)/norm) + 0.5*(p2 + p1)
+        assert points.shape == (size, 3)
+        weight = norm/(size-1)
+        weights = np.empty(size)
+        weights.fill(weight)
+        IntGrid.__init__(self, points, weights)
+
+    def _get_p1(self):
+        '''The first point of the line segment'''
+        return self._p1.view()
+
+    p1 = property(_get_p1)
+
+    def _get_p2(self):
+        '''The second point of the line segment'''
+        return self._p2.view()
+
+    p2 = property(_get_p2)
+
+    def _get_x(self):
+        '''The 1D axis for the grid points, useful for plotting
+
+           :math:`p_1` and :math:`p_2` correspond to :math:`x_1=-|p_2-p_1|/2`
+           and :math:`x_2=+|p_2-p_1|/2`, respectively.
+        '''
+        return self._x.view()
+
+    x = property(_get_x)
 
 
 class RectangleGrid(IntGrid):
-    def __init__(self, center, axis0, axis1, l0, h0, l1, h1):
+    '''A 2D rectungular grid in 3D space'''
+    def __init__(self, origin, axis0, axis1, l0, h0, l1, h1):
+        '''
+           **Arguments:**
+
+           origin
+                The origin for the definition of the rectangle.
+
+           axis0, axis1
+                The basis vectors that define the plane of the rectangle and
+                the 2D space in this plane.
+
+           l0, h0
+                The lowest and highest position along axis0. (These must be
+                integers.) Hence, along the first axis, there are (h0-l0+1)
+                grid points.
+
+           l1, h1
+                The lowest and highest position along axis1.
+        '''
         if h0 <= l0:
             raise ValueError('l0 should be lower than h0.')
         if h1 <= l1:
             raise ValueError('l1 should be lower than h1.')
 
-        # TODO: make read-only
-        self.center = center
-        self.axis0 = axis0
-        self.axis1 = axis1
-        self.l0 = l0
-        self.h0 = h0
-        self.l1 = l1
-        self.h1 = h1
-        # TODO: do not work with flat array, needs changes in base class
-        #self.shape = (h0-l0+1, h1-l1+1)
+        self._origin = origin
+        self._axis0 = axis0
+        self._axis1 = axis1
+        self._l0 = l0
+        self._h0 = h0
+        self._l1 = l1
+        self._h1 = h1
 
-        npoint = (h0-l0+1)*(h1-l1+1)
-        points = np.zeros((npoint, 3), float)
-        weights = np.empty(npoint, float)
+        size = (h0-l0+1)*(h1-l1+1)
+        points = np.zeros((size, 3), float)
+        weights = np.empty(size, float)
         weights[:] = np.sqrt(
             (np.linalg.norm(axis0)*np.linalg.norm(axis1))**2 -
             np.dot(axis0, axis1)**2
@@ -57,6 +126,70 @@ class RectangleGrid(IntGrid):
         counter = 0
         for i0 in xrange(l0, h0+1):
             for i1 in xrange(l1, h1+1):
-                points[counter] = center + axis0*i0 + axis1*i1
+                points[counter] = origin + axis0*i0 + axis1*i1
                 counter += 1
         IntGrid.__init__(self, points, weights)
+
+    def _get_origin(self):
+        '''The origin for the definition of the rectangle'''
+        return self._origin.view()
+
+    origin = property(_get_origin)
+
+    def _get_axis0(self):
+        '''The first basis vector'''
+        return self._axis0.view()
+
+    axis0 = property(_get_axis0)
+
+    def _get_axis1(self):
+        '''The second basis vector'''
+        return self._axis1.view()
+
+    axis1 = property(_get_axis1)
+
+    def _get_l0(self):
+        '''The lowest index along axis0'''
+        return self._l0
+
+    l0 = property(_get_l0)
+
+    def _get_h0(self):
+        '''The highest index along axis0'''
+        return self._h0
+
+    h0 = property(_get_h0)
+
+    def _get_l1(self):
+        '''The lowest index along axis1'''
+        return self._l1
+
+    l1 = property(_get_l1)
+
+    def _get_h1(self):
+        '''The highest index along axis1'''
+        return self._h1
+
+    h1 = property(_get_h1)
+
+    def prepare_contour(self, data):
+        '''Returns arguments suitable for matplotlib.pyplot.contour
+
+           **Arguments:**
+
+           data
+                A vector with the correct number of elements (self.size)
+
+           **Returns:** suitable arguments for  matplotlib.pyplot.contour
+
+           x, y
+                A numpy vector with the x and y axes (measured in units defined
+                by the lengths of axis0 and axis1, respectively.
+
+           z
+                the same as data, transformed in a suitable 2D numpy array
+        '''
+        x = np.arange(self.l0, self.h0+1)*np.linalg.norm(self.axis0)
+        y = np.arange(self.l1, self.h1+1)*np.linalg.norm(self.axis1)
+        z = data.reshape((self.h0 - self.l0 + 1, self.h1 - self.l1 + 1))
+        return x, y, z
