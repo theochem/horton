c18eec9ce25f83fa0ea260290e76bce49508be00
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2014-04-01 11:40:20 +0200

    Logical order of functionals in docu

--- doc/updatefunctionals.py
+++ doc/updatefunctionals.py
@@ -13,6 +13,47 @@ with open('../depends/libxc-%s/src/funcs_key.c' % libxc_version) as f:
             if len(key) > 0 and 'mgga' not in key:
                 keys.append(key)
 
+# sort the functions
+
+splitkeys = []
+for key in keys:
+    words = key.split('_')
+    if words[0] == 'hyb':
+        prefix = '_'.join(words[:2])
+        mid = words[2]
+        suffix = '_'.join(words[3:])
+    else:
+        prefix = words[0]
+        mid = words[1]
+        suffix = '_'.join(words[2:])
+    splitkeys.append((prefix, mid, suffix))
+
+def cmp_prefix(prefix1, prefix2):
+    l = ['lda', 'gga', 'hyb_gga']
+    pos1 = l.index(prefix1)
+    pos2 = l.index(prefix2)
+    return cmp(pos1, pos2)
+
+def cmp_middle(middle1, middle2):
+    l = ['k', 'x', 'c', 'xc']
+    pos1 = l.index(middle1)
+    pos2 = l.index(middle2)
+    return cmp(pos1, pos2)
+
+def cmp_splitkey(sk1, sk2):
+    result = cmp_prefix(sk1[0], sk2[0])
+    if result == 0:
+        result = cmp_middle(sk1[1], sk2[1])
+    if result == 0:
+        result = cmp(sk1[2], sk2[2])
+    return result
+
+splitkeys.sort(cmp=cmp_splitkey)
+keys = []
+for splitkey in splitkeys:
+    splitkey = [part for part in splitkey if len(part) > 0]
+    keys.append('_'.join(splitkey))
+
 # make a rst table of all functionals
 
 from horton import LibXCWrapper
