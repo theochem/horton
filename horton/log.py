773f1871d5a99be2419606ca9bf8d720c6f96687
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2014-04-01 11:26:20 +0200

    Welcome Kasia (again)

--- horton/log.py
+++ horton/log.py
@@ -491,8 +491,8 @@ head_banner = """\
  |_''_|  Horton is written and maintained by by Toon Verstraelen (1).
 
          This version contains contributions from Ali Malek (2),
-         Cristina Gonzalez (2), Farnaz H. Zadeh (2), Matthew Chan (2), Peter A.
-         Limacher (2) and Steven Vandenrbande (1).
+         Cristina Gonzalez (2), Farnaz H. Zadeh (2), Katharina Boguslawski (2),
+         Matthew Chan (2), Peter A. Limacher (2) and Steven Vandenrbande (1).
 
          (1) Center for Molecular Modeling, Ghent University, Belgium.
          (2) The Ayers Group at McMaster University, Ontario, Canada.
