Fix a lot of issues with rotation of Cartesian multipoles

horton/meanfield/rotate.py
@@ -22,7 +22,8 @@
 
 
 import numpy as np
-from horton.moments import rotate_moments_low, cartesian_transforms
+from horton.gbasis.cext import fac2
+from horton.moments import rotate_cartesian_multipole, get_cartesian_powers
 
 
 __all__ = ['rotate_coeffs']
@@ -45,19 +46,37 @@ def rotate_coeffs(coeffs, obasis, rmat):
     '''
     if obasis.nbasis != coeffs.shape[0]:
         raise TypeError('The shape of the coefficients array does not match the basis set size')
+    if obasis.shell_types.min() < 0:
+        raise TypeError('Pure functions are not supported in rotate_coeffs.')
+
     result = np.zeros(coeffs.shape)
 
-    ibasis0 = 0
+    # 1) undo the part normalization of the basis functions due to the cartesian powers
+    lmax = obasis.shell_types.max()
+    powers = get_cartesian_powers(lmax)
+    factors = []
     for ishell in xrange(obasis.nshell):
         shell_type = obasis.shell_types[ishell]
-        if shell_type < 0:
-            raise TypeError('Pure functions are not supported in rotate_coeffs.')
         icart0 = ((shell_type+2)*(shell_type+1)*(shell_type))/6
         shellsize = ((shell_type+2)*(shell_type+1))/2
         for ifn in xrange(shellsize):
-            rules = cartesian_transforms[icart0+ifn]
-            for iorb in xrange(coeffs.shape[1]):
-                result[ibasis0+ifn, iorb] = rotate_moments_low(rules, rmat, coeffs[ibasis0:ibasis0+shellsize, iorb])
+            ipow = icart0+ifn
+            factors.append(np.sqrt(fac2(2*powers[ipow,0]-1)*fac2(2*powers[ipow,1]-1)*fac2(2*powers[ipow,2]-1)))
+    factors = np.array(factors)
+    # replace the array coeffs by the one with undone normalization
+    coeffs = coeffs/factors.reshape(-1,1)
+
+    # 2) the actual rotation
+    ibasis0 = 0
+    for ishell in xrange(obasis.nshell):
+        shell_type = obasis.shell_types[ishell]
+        icart0 = ((shell_type+2)*(shell_type+1)*(shell_type))/6
+        shellsize = ((shell_type+2)*(shell_type+1))/2
+        for iorb in xrange(coeffs.shape[1]):
+            result[ibasis0:ibasis0+shellsize, iorb] = rotate_cartesian_multipole(rmat, coeffs[ibasis0:ibasis0+shellsize, iorb], 'coeffs')
         ibasis0 += shellsize
+
+    # 3) apply the part of the normalization of the basis functions due to the cartesian powers
+    result *= factors.reshape(-1,1)
 
     return result

horton/moments.py
@@ -31,7 +31,8 @@ import numpy as np
 
 
 __all__ = ['get_cartesian_powers', 'get_ncart', 'get_ncart_cumul',
-           'rotate_cartesian_moments', 'get_npure', 'get_npure_cumul']
+           'rotate_cartesian_multipole', 'rotate_cartesian_moments_all',
+           'get_npure', 'get_npure_cumul']
 
 
 def get_cartesian_powers(lmax):
@@ -69,22 +70,11 @@ def get_ncart_cumul(lmax):
     return ((lmax+1)*(lmax+2)*(lmax+3))/6
 
 
-
-# Each item in the following list:
-#  - defines how a cartesian multipole is transformed.
-#  - corresponds to the multipole moments defined by get_cartesian_powers.
-#  - consists of a list of rules.
-# Each rule contains at least two integer and corresponds to a single term
-# in the linear transformation.
-#  - first an index for the multipole that contributes to rotated multipole,
-#    relative to the first moment in this shell.
-#  - second an integer linear coefficient for thus term.
-#  - then a number of indexes that refer to coefficients in the rotation matrix
-#    that occur as factors in the term.
-
-
 cartesian_transforms = [
+  [
     [[ 0,  1]],
+  ],
+  [
     [[ 0,  1,  0],
      [ 1,  1,  1],
      [ 2,  1,  2]],
@@ -94,6 +84,8 @@ cartesian_transforms = [
     [[ 0,  1,  6],
      [ 1,  1,  7],
      [ 2,  1,  8]],
+  ],
+  [
     [[ 0,  1,  0,  0],
      [ 1,  2,  0,  1],
      [ 2,  2,  0,  2],
@@ -139,6 +131,8 @@ cartesian_transforms = [
      [ 3,  1,  7,  7],
      [ 4,  2,  7,  8],
      [ 5,  1,  8,  8]],
+  ],
+  [
     [[ 0,  1,  0,  0,  0],
      [ 1,  3,  0,  0,  1],
      [ 2,  3,  0,  0,  2],
@@ -304,6 +298,8 @@ cartesian_transforms = [
      [ 7,  3,  7,  7,  8],
      [ 8,  3,  7,  8,  8],
      [ 9,  1,  8,  8,  8]],
+  ],
+  [
     [[ 0,  1,  0,  0,  0,  0],
      [ 1,  4,  0,  0,  0,  1],
      [ 2,  4,  0,  0,  0,  2],
@@ -799,61 +795,84 @@ cartesian_transforms = [
      [12,  6,  7,  7,  8,  8],
      [13,  4,  7,  8,  8,  8],
      [14,  1,  8,  8,  8,  8]],
+  ],
 ]
 
 
-def rotate_cartesian_moments(moments, rmat):
+def rotate_cartesian_multipole(rmat, moments, mode):
+    '''Compute rotated Cartesian multipole moment/expansion.
+
+       **Arguments:**
+
+       rmat
+            A (3,3) rotation matrix.
+
+       moments
+            A multipole moment/coeffs. The angular momentum is derived from the
+            length of this vector.
+
+       mode
+            A string containing either 'moments' or 'coeffs'. In case if
+            'moments', a Cartesian multipole moment rotation is carried out. In
+            case of 'coeffs', the coefficients of a Cartesian multipole basis
+            are rotated.
+
+       **Returns:** rotated multipole.
+    '''
+    l = ((9 + 8*(len(moments)-1))**0.5-3)/2
+    if l - np.round(l) > 1e-10:
+        raise ValueError('Could not determine l from number of moments.')
+    l = int(np.round(l))
+
+    if mode == 'coeffs':
+        rcoeffs = rmat.T.ravel()
+    elif mode == 'moments':
+        rcoeffs = rmat.ravel()
+    else:
+        raise NotImplementedError
+    result = np.zeros(len(moments))
+    for i0 in xrange(len(moments)):
+        rules = cartesian_transforms[l][i0]
+        for rule in rules:
+            i1 = rule[0]
+            factor = rule[1]
+            for j in rule[2:]:
+               factor *= rcoeffs[j]
+            if mode == 'coeffs':
+                result[i1] += moments[i0]*factor
+            elif mode == 'moments':
+                result[i0] += moments[i1]*factor
+            else:
+                raise NotImplementedError
+    return result
+
+
+def rotate_cartesian_moments_all(rmat, moments):
     '''Rotate cartesian moments
 
        **Arguments:**
 
-       moments
-            A row vector with a series of cartesian multipole moments. Items in
-            this vector should follow the same order as defined by the function
-            ``get_cartesian_powers``.
-
        rmat
             A (3,3) rotation matrix.
+
+       moments
+            A row vector with a series of cartesian multipole moments, starting
+            from l=0 up to l=lmax. Items in this vector should follow the same
+            order as defined by the function ``get_cartesian_powers``.
+
+       **Returns:** A similar vector with rotated multipole moments
     '''
     ncart = moments.shape[0]
     result = np.zeros(ncart)
 
-    icart0 = 0
+    icart = 0
     nshell = 1
-    counter = 2
-    for icart in xrange(ncart):
-        if icart >= icart0 + nshell:
-            icart0 += nshell
-            nshell += counter
-            counter += 1
-        result[icart] = rotate_moments_low(cartesian_transforms[icart], rmat, moments[icart0:icart0+nshell])
-    return result
-
-
-def rotate_moments_low(rules, rmat, moments):
-    '''Return rotated a multipole based on the given rules
-
-       **Arguments:**
-
-       moments
-            A row vector with a series of cartesian multipole moments. Items in
-            this vector should follow the same order as defined by the function
-            ``get_cartesian_powers``.
-
-       rules
-            An item from the list cartesian_transforms.
-
-       rmat
-            A (3,3) rotation matrix.
-    '''
-    rcoeffs = rmat.ravel()
-    result = 0
-    for rule in rules:
-        i = rule[0]
-        factor = rule[1]
-        for j in rule[2:]:
-           factor *= rcoeffs[j]
-        result += moments[i]*factor
+    ishell = 0
+    while icart < ncart:
+        result[icart:icart+nshell] = rotate_cartesian_multipole(rmat, moments[icart:icart+nshell], 'moments')
+        icart += nshell
+        ishell += 1
+        nshell += ishell+1
     return result
 
 

horton/part/symmetry.py
@@ -23,7 +23,7 @@
 
 import numpy as np
 
-from horton.moments import rotate_cartesian_moments
+from horton.moments import rotate_cartesian_moments_all
 
 
 __all__ = ['symmetry_analysis']
@@ -79,7 +79,7 @@ def symmetry_analysis(coordinates, cell, symmetry, aim_results):
         for i, generator in enumerate(equiv_generators):
             # The multipole moments are rotated back to the primitive unit,
             # so we need to take the transpose.
-            rotated.append(rotate_cartesian_moments(equiv_values[i], generator[:,:3].T))
+            rotated.append(rotate_cartesian_moments_all(generator[:,:3].T, equiv_values[i]))
         return np.mean(rotated, axis=0), np.std(rotated, axis=0)
 
     # associate analysis functions with certain fields

horton/test/test_moments.py
@@ -64,13 +64,14 @@ def test_get_cartesian_powers():
 
 
 def test_rotate_cartesian_moments_pentagon_mult():
-    for mult in 2, 3, 4, 5, 6, 7:
+    for mult in 2, 3, 4, 5:
         axis = np.random.normal(0, 1, 3)
-        rmat = get_rotation_matrix(axis, 2.0*np.pi/mult)
+        rmat = get_rotation_matrix(axis, np.pi/mult)
         m0 = get_pentagon_moments()
         m1 = get_pentagon_moments()
         for i in xrange(mult):
-            m1 = rotate_cartesian_moments(m1, rmat)
+            m1 = rotate_cartesian_moments_all(rmat, m1)
+            m1 = rotate_cartesian_moments_all(rmat, m1)
         assert abs(m0 - m1).max() < 1e-10
 
 
@@ -79,20 +80,21 @@ def test_rotate_cartesian_moments_pentagon_general():
         rmat = get_random_rotation()
         m0 = get_pentagon_moments()
         m1 = get_pentagon_moments(rmat)
-        m2 = rotate_cartesian_moments(m0, rmat)
+        m2 = rotate_cartesian_moments_all(rmat, m0)
         assert abs(m1 - m2).max() < 1e-10
 
 
 def test_rotate_cartesian_moments_random_mult():
     for i in xrange(10):
         coordinates = np.random.normal(0, 1, (10, 3))
-        for mult in 2, 3, 4, 5, 6, 7:
+        for mult in 2, 3, 4, 5:
             axis = np.random.normal(0, 1, 3)
-            rmat = get_rotation_matrix(axis, 2.0*np.pi/mult)
+            rmat = get_rotation_matrix(axis, np.pi/mult)
             m0 = get_point_moments(coordinates)
             m1 = get_point_moments(coordinates)
             for i in xrange(mult):
-                m1 = rotate_cartesian_moments(m1, rmat)
+                m1 = rotate_cartesian_moments_all(rmat, m1)
+                m1 = rotate_cartesian_moments_all(rmat, m1)
             assert abs(m0 - m1).max() < 1e-10
 
 
@@ -102,8 +104,28 @@ def test_rotate_cartesian_moments_random_general():
         rmat = get_random_rotation()
         m0 = get_point_moments(coordinates)
         m1 = get_point_moments(coordinates, rmat)
-        m2 = rotate_cartesian_moments(m0, rmat)
+        m2 = rotate_cartesian_moments_all(rmat, m0)
         assert abs(m1 - m2).max() < 1e-10
+
+
+def test_rotate_simple_moments():
+    rmat = get_rotation_matrix(np.array([0, 0, 1]), np.pi/4)
+    m0 = np.array([1, 0, 0])
+    m1 = rotate_cartesian_multipole(rmat, m0, 'moments')
+    assert abs(m1 - [0.5**0.5, 0.5**0.5, 0.0]).max() < 1e-10
+    m0 = np.array([1, 0, 0, 0, 0, 0])
+    m1 = rotate_cartesian_multipole(rmat, m0, 'moments')
+    assert abs(m1 - [0.5, 0.5, 0, 0.5, 0.0, 0.0]).max() < 1e-10
+
+
+def test_rotate_simple_coeffs():
+    rmat = get_rotation_matrix(np.array([0, 0, 1]), np.pi/4)
+    m0 = np.array([1, 0, 0])
+    m1 = rotate_cartesian_multipole(rmat, m0, 'coeffs')
+    assert abs(m1 - [0.5**0.5, 0.5**0.5, 0.0]).max() < 1e-10
+    m0 = np.array([1, 0, 0, 0, 0, 0])
+    m1 = rotate_cartesian_multipole(rmat, m0, 'coeffs')
+    assert abs(m1 - [0.5, 1.0, 0, 0.5, 0.0, 0.0]).max() < 1e-10
 
 
 def test_fill_cartesian_polynomials():