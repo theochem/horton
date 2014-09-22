5757d65f5655a130c1b71977bb158b5dada4cc39
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2014-09-22 16:06:01 +0200

    Cleaner cleancode.sh

--- cleancode.sh
+++ cleancode.sh
@@ -2,6 +2,7 @@
 echo Cleaning python code in \'`pwd`\' and subdirectories
 for file in $(find data doc horton tools scripts examples *.py *.sh | egrep "(\.rst$)|(README)|(\.bib$)|(\.py$)|(\.c$)|(\.h$)|(\.nwchem)|(\.pyx$)|(\.pxd$)|(\.cpp)|(\.sh)"); do
   echo Cleaning ${file}
+  dos2unix -q ${file}
   sed -i -e $'s/\t/    /g' ${file}
   sed -i -e $'s/[ \t]\+$//g' ${file}
   sed -i -e :a -e '/^\n*$/{$d;N;ba' -e '}' ${file}
