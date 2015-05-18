c8a26692763fd0a387d7acb2eac645bb613fad47
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2015-05-18 15:42:01 -0700

    Remove outdated file

--- customize.py
+++
@@ -1,6 +0,0 @@
-# If you want to link against a custom libint and libxc, uncomment and change
-# the lines below. Note that only static linking is supported. Header files
-# must also be present.
-
-#libintdir = 'depends/libint-2.0.3-stable'
-#libxcdir = 'depends/libxc-2.1.0'
