8ada1409de4865d0991745334f557d114279b80f
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2015-05-24 14:36:14 -0700

    Integrate FCIDUMP with Molecule class

--- data/examples/ap1rog/extham.py
+++ data/examples/ap1rog/extham.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python
 
 from horton import *
+
+###############################################################################
+## Read Hamiltonian from file 'FCIDUMP' #######################################
+###############################################################################
+mol = Molecule.from_file('./FCIDUMP')
+
 ###############################################################################
 ## Define number of occupied orbitals and total number of basis functions #####
 ###############################################################################
 nocc = 5
-nbasis = 28
+
 ###############################################################################
 ## Define Occupation model, expansion coefficients and overlap ################
 ###############################################################################
-lf = DenseLinalgFactory(nbasis)
 occ_model = AufbauOccModel(nocc)
-orb = lf.create_expansion(nbasis)
-olp = lf.create_two_index(nbasis)
+orb = mol.lf.create_expansion()
+olp = mol.lf.create_two_index()
 olp.assign_diagonal(1.0)
 orb.assign(olp)
-###############################################################################
-## Read Hamiltonian from file 'FCIDUMP' #######################################
-###############################################################################
-one, two, core = load_fcidump(lf, './FCIDUMP')
 
 ###############################################################################
 ## Do OO-AP1roG optimization ##################################################
 ###############################################################################
-ap1rog = RAp1rog(lf, occ_model)
-energy, c, l = ap1rog(one, two, core, orb, olp, True)
+ap1rog = RAp1rog(mol.lf, occ_model)
+energy, c, l = ap1rog(mol.one_mo, mol.two_mo, mol.core_energy, orb, olp, True)
