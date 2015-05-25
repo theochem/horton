fc3138f1621374d67ebb1a4b84cfbac3a7a888b2
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2015-05-25 08:22:49 -0700

    Add test for AP1roG restart

--- horton/correlatedwfn/test/test_geminals.py
+++ horton/correlatedwfn/test/test_geminals.py
@@ -21,36 +21,41 @@
 
 
 from horton import *
+from horton.test.common import tmpdir
 import numpy as np
 
 
-def prepare_hf(basis):
+def prepare_mol(basis, scale=1.0):
     fn_xyz = context.get_fn('test/h2.xyz')
     mol = Molecule.from_file(fn_xyz)
+    mol.coordinates *= scale
     obasis = get_gobasis(mol.coordinates, mol.numbers, basis)
     lf = DenseLinalgFactory(obasis.nbasis)
-    occ_model = AufbauOccModel(1)
-    exp_alpha = lf.create_expansion(obasis.nbasis)
     olp = obasis.compute_overlap(lf)
     kin = obasis.compute_kinetic(lf)
     na = obasis.compute_nuclear_attraction(mol.coordinates, mol.pseudo_numbers, lf)
-    guess_core_hamiltonian(olp, kin, na, exp_alpha)
-
+    one = kin.copy()
+    one.iadd(na)
     er = obasis.compute_electron_repulsion(lf)
+
+    return mol, lf, olp, one, er
+
+
+def prepare_hf(basis, scale=1.0):
+    mol, lf, olp, one, er = prepare_mol(basis, scale)
+
+    exp_alpha = lf.create_expansion()
+    guess_core_hamiltonian(olp, one, exp_alpha)
     external = {'nn': compute_nucnuc(mol.coordinates, mol.pseudo_numbers)}
     terms = [
-        RTwoIndexTerm(kin, 'kin'),
+        RTwoIndexTerm(one, 'one'),
         RDirectTerm(er, 'hartree'),
         RExchangeTerm(er, 'x_hf'),
-        RTwoIndexTerm(na, 'ne'),
     ]
     ham = REffHam(terms, external)
+    occ_model = AufbauOccModel(1)
     scf_solver = PlainSCFSolver()
     scf_solver(ham, lf, olp, occ_model, exp_alpha)
-
-    one = lf.create_two_index(obasis.nbasis)
-    one.iadd(kin)
-    one.iadd(na)
 
     return lf, occ_model, one, er, external, exp_alpha, olp
 
@@ -73,3 +78,36 @@ def test_ap1rog_cs_scf():
     guess = np.array([-0.1, -0.05, -0.02])
     energy, g, l = geminal_solver(one, er, external['nn'], exp_alpha, olp, True, **{'checkpoint': -1, 'guess': {'geminal': guess}})
     assert (abs(energy - -1.151686291339) < 1e-6)
+
+
+def test_ap1rog_cs_scf_restart():
+    lf, occ_model, one, er, external, exp_alpha, olp = prepare_hf('6-31G')
+
+    # Do AP1roG optimization:
+    geminal_solver = RAp1rog(lf, occ_model)
+    guess = np.array([-0.1, -0.05, -0.02])
+
+    with tmpdir('horton.correlatedwfn.test.test_geminals.test_ap1rog_cs_scf_restart') as dn:
+        checkpoint_fn = '%s/checkpoint.h5' % dn
+        energy, g, l = geminal_solver(one, er, external['nn'], exp_alpha, olp,
+                                      True, guess={'geminal': guess},
+                                      checkpoint_fn=checkpoint_fn)
+        assert (abs(energy - -1.151686291339) < 1e-6)
+
+        old = Molecule.from_file(checkpoint_fn)
+
+    assert hasattr(old, 'olp')
+    assert hasattr(old, 'exp_alpha')
+
+    # Update to slightly stretch geometry of H2
+    mol1, lf, olp1, one1, er1 = prepare_mol('6-31G', 1.05)
+
+    # re-orthogonalize orbitals
+    project_orbitals_ortho(old.olp, olp1, old.exp_alpha, exp_alpha)
+
+    # recompute
+    energy, c, l = geminal_solver(one1, er1, external['nn'], exp_alpha, olp1,
+                                  True, guess={'geminal': guess},
+                                  checkpoint=-1)
+
+    assert (abs(energy - -1.11221603918) < 1e-6)
