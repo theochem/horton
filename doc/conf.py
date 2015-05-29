f415434b08b48760c27b0068949aff15e0b99bf4
Matthew Chan <chanmhy@mcmaster.ca>
2015-05-29 02:16:35 -0400

    moved to mathjax for pixelation free math

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,8 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.pngmath', 'breathe']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.mathjax', 'breathe']
+mathjax_path = "http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
