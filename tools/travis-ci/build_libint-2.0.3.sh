28eebd6346f785d7b3db01e242832863b3b54e33
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2015-07-14 06:30:06 +0200

    Fix scripts for Travis CI
    - All depedencies are built with version numbers as documented
    - Most dependencies are cached, which speeds up testing
    - Documentation build is also tested

---
+++ tools/travis-ci/build_libint-2.0.3.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# exit script when a line fails (instead of going to the next line)
+set -e
+
+COLOR='\033[0;32m'
+RESET='\033[0m'
+
+# check to see if libint-2.0.3 is already present
+if [ ! -d "$HOME/depinstall/libint-2.0.3/lib" ]; then
+(
+    echo -e "${COLOR}Building and installing LIBINT 2.0.3 from scratch${RESET}"
+    cd
+    mkdir -p depbuild
+    cd depbuild
+    wget 'http://downloads.sourceforge.net/project/libint/libint-for-mpqc/libint-2.0.3-stable.tgz'
+    tar -xzf libint-2.0.3-stable.tgz
+    cd libint-2.0.3-stable
+    echo "Actual build and install. This may take a while."
+    (CONFIG_SHELL=$(which sh) ./configure --prefix=${HOME}/depinstall/libint-2.0.3 --enable-shared && make install) &> install.log
+    tail install.log
+)
+else
+    echo -e "${COLOR}Using Cached LIBINT 2.0.3${RESET}"
+fi
+
+find $HOME/depinstall/libint-2.0.3
