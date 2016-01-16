eff0f075050b77be1caf367917ce4e1751c64dda
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2016-01-16 14:20:16 +0100

    Fix matplotlib DISPLAY issue again

--- tools/qa/deps/common.sh
+++ tools/qa/deps/common.sh
@@ -8,3 +8,7 @@ RESET='\e[0m'
 # Make sure there is a ${QAWORKDIR}
 [[ -z ${QAWORKDIR} ]] && export QAWORKDIR=${PWD}/qaworkdir
 [[ -d ${QAWORKDIR} ]] || mkdir -p ${QAWORKDIR}
+
+# Make sure matplotlib does not complain about DISPLAY
+export MATPLOTLIBRC=${QAWORKDIR}
+echo "backend: agg" > $MATPLOTLIBRC/matplotlibrc
