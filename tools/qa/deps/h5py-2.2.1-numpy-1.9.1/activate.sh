ef51598498d8b3ccb80449480fdd9de8589ad1ea
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2016-01-16 06:23:43 +0100

    Use more PIP for installing dependencies

--- tools/qa/deps/h5py-2.2.1-numpy-1.9.1/activate.sh
+++
@@ -1,6 +0,0 @@
-NAMEVER=$(basename $(dirname "${BASH_SOURCE[0]}"))
-source tools/qa/deps/common.sh
-if [ -d "${QAWORKDIR}/depinstall/${NAMEVER}/lib/python2.7/site-packages" ]; then
-    echo -e "${GREEN}Activating ${NAMEVER}${RESET}"
-    export PYTHONPATH=${QAWORKDIR}/depinstall/${NAMEVER}/lib/python2.7/site-packages:${PYTHONPATH}
-fi
