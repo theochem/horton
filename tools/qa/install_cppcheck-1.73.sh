c8449436c6ccbe09341ac692fba20f638869b103
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2016-06-03 09:02:43 +0200

    Build our own cppcheck
    This will build cppcheck on Travis/buildbot and cache the results.
    When present, this binary gets picked up automatically by
    trapdoor_cppcheck.py

---
+++ tools/qa/install_cppcheck-1.73.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+VERSION=1.73
+NAMEVER=cppcheck-${VERSION}
+set -e
+source tools/qa/common.sh
+if [ ! -d "${CACHED}/${NAMEVER}/cppcheck" ]; then
+(
+    echo -e "${GREEN}Building and installing ${NAMEVER} from scratch${RESET}"
+    cd ${QAWORKDIR}
+    mkdir -p depbuild
+    cd depbuild
+    curl -OL "http://downloads.sourceforge.net/project/cppcheck/cppcheck/${VERSION}/${NAMEVER}.tar.bz2"
+    tar -xjf ${NAMEVER}.tar.bz2
+    cd ${NAMEVER}
+    echo "Actual build and install. This may take a while."
+    make SRCDIR=build CFGDIR=${CACHED}/${NAMEVER}/cfg HAVE_RULES=yes CXXFLAGS="-O2 -DNDEBUG -Wall -Wno-sign-compare -Wno-unused-function" &> install.log
+    tail install.log
+    mkdir -p ${CACHED}/${NAMEVER}
+    cp -av cppcheck ${CACHED}/${NAMEVER}
+    cp -av cfg ${CACHED}/${NAMEVER}
+)
+else
+    echo -e "${GREEN}Using Cached ${NAMEVER}${RESET}"
+fi
