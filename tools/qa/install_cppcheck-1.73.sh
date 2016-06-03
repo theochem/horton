c25baf5f1c01b61332fff5d78aa4b442238a0079
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2016-06-03 17:20:37 +0200

    Fix another mistake in install_cppcheck

--- tools/qa/install_cppcheck-1.73.sh
+++ tools/qa/install_cppcheck-1.73.sh
@@ -16,7 +16,7 @@ if [ ! -d "${CACHED}/${NAMEVER}/cppcheck" ]; then
     echo "Actual build and install. This may take a while."
     make SRCDIR=build CFGDIR=${CACHED}/${NAMEVER}/cfg HAVE_RULES=yes CXXFLAGS="-O2 -DNDEBUG -Wall -Wno-sign-compare -Wno-unused-function" &> install.log
     tail install.log
-    mkdir ${CACHED}/${NAMEVER}
+    mkdir -p ${CACHED}/${NAMEVER}
     cp -av cppcheck ${CACHED}/${NAMEVER}
     cp -av cfg ${CACHED}/${NAMEVER}
 )
