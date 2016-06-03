3adaf9a4ac34790cfbf1ec4b178020e7d72a68a3
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2016-06-03 18:20:44 +0200

    Detect duplicate entries when loading dependencies.txt

--- doc/update_install_doc.py
+++ doc/update_install_doc.py
@@ -76,10 +76,21 @@ def formatter_macports_command(d):
         return '    %s' % (macports_command)
 
 
+def dict_raise_on_duplicates(ordered_pairs):
+    """Reject duplicate keys when loading JSON data."""
+    d = {}
+    for k, v in ordered_pairs:
+        if k in d:
+           raise ValueError("duplicate key: %r" % (k,))
+        else:
+           d[k] = v
+    return d
+
+
 def collect_fields():
     # Load the dependencies data
     with open('../dependencies.txt') as f:
-        dependencies = json.load(f)
+        dependencies = json.load(f, object_pairs_hook=dict_raise_on_duplicates)
 
     result = {}
 
