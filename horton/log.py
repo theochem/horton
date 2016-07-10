d0e0d763a0ce5da6130f515a5a9e4e39d52f88ca
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2016-07-10 07:52:49 -0500

    Document most of horton.log.
    Parts slated for removal are not documented.

--- horton/log.py
+++ horton/log.py
@@ -18,15 +18,14 @@
 # along with this program; if not, see <http://www.gnu.org/licenses/>
 #
 # --
-'''Screen logging, timing and citation management
+"""Screen logging, timing and citation management
 
-   The goal of the screen logger is to track the progress of a computation in
-   a convenient human-readable way, possibly higlighting problematic situations.
-   It is not intended as a computer-readable output file that contains all the
-   results of a computation. For that purpose, all useful information is
-   written to a binary checkpoint file or kept in memory as attributes of the
-   HORTON objects.
-'''
+The goal of the screen logger is to track the progress of a computation in a convenient
+human-readable way, possibly higlighting problematic situations. It is not intended as a
+computer-readable output file that contains all the results of a computation. For that
+purpose, all useful information is written to a binary checkpoint file or kept in memory
+as attributes of the HORTON objects.
+"""
 
 import sys, os, datetime, getpass, time, atexit, traceback, resource, urllib
 from contextlib import contextmanager
@@ -39,7 +38,24 @@ __all__ = ['log', 'timer']
 
 
 class ScreenLog(object):
-    # log levels
+    """Nice and configurable screen output.
+
+    The screen logger can be configured to print more or less output, with a parameter
+    called the *log level*. This can be one of the class attributes:
+
+    - `silent`: nothing at all
+    - `warning`: only report things that may be going wrong
+    - `low`: terse screen output
+    - `medium`: normal screen output (default)
+    - `high`: plenty of detail
+    - `debug`: too much detail for a regular user
+
+    Output is printed by `ScreenLog` with its `__call__` method (and also a few others).
+    The program that uses `ScreenLog` is responsible for checking the log level before
+    generating screen output with the `do_*` properties.
+    """
+
+    # Log levels.
     silent = 0
     warning = 1
     low = 2
@@ -47,10 +63,25 @@ class ScreenLog(object):
     high = 4
     debug = 5
 
-    # screen parameter
+    # Screen width parameter.
     width = 100
 
     def __init__(self, name, version, head_banner, foot_banner, timer, f=None):
+        """Initialize a ScreenLog object.
+
+        Parameters
+        ----------
+        name : str
+            Name of the program.
+        version : str
+            Version number of the program.
+        head_banner : str
+            The first text to be printed on screen.
+        foot_banner : str
+            The last test to be printed on screen.
+        timer : Timer
+            A timer to keep track of CPU time taken by different parts of the program.
+        """
         self.name = name
         self.version = version
         self.head_banner = head_banner
@@ -69,6 +100,8 @@ class ScreenLog(object):
             _file = f
         self._file = _file
 
+    # The following properties can be used by the program to see if output of a given
+    # priority should be printed.
     do_warning = property(lambda self: self._level >= self.warning)
     do_low = property(lambda self: self._level >= self.low)
     do_medium = property(lambda self: self._level >= self.medium)
@@ -76,14 +109,30 @@ class ScreenLog(object):
     do_debug = property(lambda self: self._level >= self.debug)
 
     def set_level(self, level):
+        """Set the log level.
+
+        Parameters
+        ----------
+        level : int
+            The desired level of output.
+        """
         if level < self.silent or level > self.debug:
             raise ValueError('The level must be one of the ScreenLog attributes.')
         self._level = level
 
     def with_level(self, level):
+        """Decorator to change the log level inside a specific function.
+
+        Parameters
+        ----------
+        level : int
+            The desired level of output.
+        """
         def decorator(fn):
+            """Return a wrapped function in which the log level is set to the desired value."""
             @wraps(fn)
             def wrapper(*args, **kwargs):
+                """The wrapper."""
                 old_level = self._level
                 self.set_level(level)
                 try:
@@ -95,6 +144,11 @@ class ScreenLog(object):
         return decorator
 
     def __call__(self, *words):
+        """Print nicely formatted output to screen.
+
+        All arguments are joined with a spaced and wrapped to a fixed line width before
+        printing. The first ampersand is interpreted as the place of a hanging indent.
+        """
         s = ' '.join(w for w in words)
         if not self.do_warning:
             raise RuntimeError('The runlevel should be at least warning when logging.')
@@ -135,15 +189,31 @@ class ScreenLog(object):
         self._last_blank = False
 
     def warn(self, *words):
+        """Format and print a warning.
+
+        See `__call__` for more details.
+        """
         self.blank()
         text = '!WARNING!&'+' '.join(w for w in words)
         self(text)
         self.blank()
 
     def hline(self, char='~'):
+        """Print a horizontal line.
+
+        Parameters
+        ----------
+        char : str
+            The line consists a repetition of this character.
+        """
         self(char*self.width)
 
     def center(self, *words, **kwargs):
+        """Print centered text, optionally with an edge surrounding the text.
+
+        See __call__ for the details. One keyword argument `edge` is accepted that will be
+        printed to the left and the right of the centered text.
+        """
         if len(kwargs) == 0:
             edge = ''
         elif len(kwargs) == 1:
@@ -158,28 +228,64 @@ class ScreenLog(object):
         self('%s%s%s' % (edge, s.center(self.width-2*len(edge)), edge))
 
     def blank(self):
+        """Print a blank line."""
         if not self._last_blank:
             print >> self._file
             self._last_blank = True
 
     def deflist(self, l):
+        """Print a definition list.
+
+        Parameters
+        ----------
+        l : list
+            A list of keyword and value pairs. A table will be printed where the first
+            column contains the keywords and the second colum contains (wrapped) values.
+        """
         widest = max(len(item[0]) for item in l)
         for name, value in l:
             self('  %s :&%s' % (name.ljust(widest), value))
 
     def cite(self, key, reason):
+        """Cite a reference from `data/references.bib` for given reason.
+
+        Parameters
+        ----------
+        key : str
+            The bibtex key in `data/references.bib`.
+        reason: str
+            The reason why this reference is cited, e.g. something in the form of
+            `"for using method bluh"`. You may want to cite the same reference for
+            different reasons from different parts of the code.
+
+        At the end of the program, a list of "relevant" references will be printed that
+        one should cite when using results obtained with a HORTON calculation.
+        """
         if self._biblio is None:
             filename = context.get_fn('references.bib')
             self._biblio = Biblio(filename)
         self._biblio.cite(key, reason)
 
     def progress(self, niter):
-        # Only make the progress bar active at the medium level
+        """Return a progress bar for `niter` iterations.
+
+        The progress bar can be used as follows:
+
+        .. code-block:: python
+
+            pb = log,progress(100)
+            for i in xrange(100):
+                pb()
+
+        A progress bar is only active at the medium level.
+        """
         return ProgressBar(niter, self._file, self.width, silent=self._level != self.medium)
 
     def print_header(self):
+        """Print the first screen output."""
         # Suppress any logging as soon as an exception is not caught.
         def excepthook_wrapper(type, value, traceback):
+            """Silence the logger (as soon as an exception is raised)."""
             self.set_level(self.silent)
             sys.__excepthook__(type, value, traceback)
         sys.excepthook = excepthook_wrapper
@@ -190,6 +296,7 @@ class ScreenLog(object):
             self._print_basic_info()
 
     def print_footer(self):
+        """Print the last screen output."""
         if self.do_warning and self._active:
             self._print_references()
             self._print_basic_info()
@@ -198,10 +305,12 @@ class ScreenLog(object):
             print >> self._file, self.foot_banner
 
     def _print_references(self):
+        """Print all cited references."""
         if self._biblio is not None:
             self._biblio.log()
 
     def _print_basic_info(self):
+        """Print basic runtime info."""
         if self.do_low:
             self.blank()
             self('User:          &' + getpass.getuser())
@@ -216,7 +325,22 @@ class ScreenLog(object):
 
 
 class ProgressBar(object):
+    """Sinple progress bar for the screen logger."""
+
     def __init__(self, niter, f, width, silent):
+        """Initialize a ProgressBar object.
+
+        Parameters
+        ----------
+        niter : int
+            The number of iterations to reach completion of something iterative.
+        f : file object
+            The file to write the progress bar to.
+        width : int
+            The screen width.
+        silent : bool
+            When `True`, nothing gets printed.
+        """
         self.niter = niter
         self.f = f
         self.width = width
@@ -225,6 +349,7 @@ class ProgressBar(object):
         self.nchar = 0
 
     def __call__(self, inc=1):
+        """Increment the progress bar counter."""
         self.count += inc
         if not self.silent:
             new_nchar = (self.count*self.width)/self.niter
@@ -238,21 +363,29 @@ class ProgressBar(object):
             raise ValueError('Progress bar overflow.')
 
 
-
 class Timer(object):
+    """Keep track of CPU time of a part of the program.
+
+    This is the bare-bones timer. It does not make any distinction between time spent in
+    its own code or code of other functions it calls (if they are also timed).
+    """
+
     def __init__(self):
+        """Initialize the Timer object."""
         self.cpu = 0.0
         self._start = None
         # The _depth attribute is needed for timed recursive functions.
         self._depth = 0
 
     def start(self):
+        """Mark start of a function."""
         if self._depth == 0:
             assert self._start is None
             self._start = time.clock()
         self._depth += 1
 
     def stop(self):
+        """Mark end of a function."""
         if self._depth > 0:
             assert self._start is not None
             self._depth -= 1
@@ -262,39 +395,72 @@ class Timer(object):
 
 
 class SubTimer(object):
+    """Keep track of CPU time in a part of a program, aware of timed called functions."""
+
     def __init__(self, label):
+        """Initialize a SubTimer object.
+
+        Parameters
+        ----------
+        label : str
+            A name for this timer.
+        """
         self.label = label
         self.total = Timer()
         self.own = Timer()
 
     def start(self):
+        """Mark start of the timed function."""
         self.total.start()
         self.own.start()
 
     def start_sub(self):
+        """Mark start of a timed function inside the current function."""
         self.own.stop()
 
     def stop_sub(self):
+        """Mark end of a timed function inside the current function."""
         self.own.start()
 
     def stop(self):
+        """Mark end of the timed function."""
         self.own.stop()
         self.total.stop()
 
 
 class TimerGroup(object):
+    """Keep track of CPU time spent in different parts of a code."""
+
     def __init__(self):
+        """Initialize a TimerGroup object."""
         self.parts = {}
         self._stack = []
         self._start('Total')
 
     def reset(self):
+        """Reset all timers."""
         for timer in self.parts.itervalues():
             timer.total.cpu = 0.0
             timer.own.cpu = 0.0
 
     @contextmanager
     def section(self, label):
+        """Time part of a code with a contextmanager.
+
+        Parameters
+        ----------
+        label : str
+            A label for that part of the code.
+
+        This can be used as follows:
+
+        .. code-block:: python
+
+            with log.timer('AlgoWhatever'):
+                # Computationally expensive code here, instead of pass:
+                pass
+            # Back to code that is has no significant cost.
+        """
         self._start(label)
         try:
             yield
@@ -302,15 +468,43 @@ class TimerGroup(object):
             self._stop(label)
 
     def with_section(self, label):
+        """Time part of a code with a decorator.
+
+        Parameters
+        ----------
+        label : str
+            A label for that part of the code.
+
+        This can be used as follows:
+
+        .. code-block:: python
+
+            @log.timer.with_section('AlgoThisAndThat')
+            def slow_function(*args, **kwargs):
+                # Slow code inside...
+                pass
+        """
         def decorator(fn):
+            """Decorator that adds timer to function."""
             @wraps(fn)
             def wrapper(*args, **kwargs):
+                """Wrap function with timer."""
                 with self.section(label):
                     return fn(*args, **kwargs)
             return wrapper
         return decorator
 
     def _start(self, label):
+        """Start timing part of a code.
+
+        This method should not be called directly. Use decorator and contextmanager API
+        instead.
+
+        Parameters
+        ----------
+        label : str
+            A label for that part of the code.
+        """
         assert len(label) <= 14
         # get the right timer object
         timer = self.parts.get(label)
@@ -325,6 +519,16 @@ class TimerGroup(object):
         self._stack.append(timer)
 
     def _stop(self, label):
+        """Stop timing part of a code.
+
+        This method should not be called directly. Use decorator and contextmanager API
+        instead.
+
+        Parameters
+        ----------
+        label : str
+            A label for that part of the code.
+        """
         timer = self._stack.pop(-1)
         assert timer.label == label
         timer.stop()
@@ -332,6 +536,7 @@ class TimerGroup(object):
             self._stack[-1].stop_sub()
 
     def get_max_own_cpu(self):
+        """Return the CPU time of the slowest part of the code."""
         result = None
         for part in self.parts.itervalues():
             if result is None or result < part.own.cpu:
@@ -339,6 +544,13 @@ class TimerGroup(object):
         return result
 
     def report(self, log):
+        """Write a report of the CPU usage on screen.
+
+        Parameters
+        ----------
+        log : ScreenLog
+            A logger to use for writing the screen output.
+        """
         max_own_cpu = self.get_max_own_cpu()
         #if max_own_cpu == 0.0:
         #    return
@@ -370,12 +582,27 @@ class TimerGroup(object):
 
 
 class Reference(object):
+    """Define a (literature) reference."""
+
     def __init__(self, kind, key):
+        """Initialize a Reference object.
+
+        Initially, the reference contains not information. Tags with actual data are added
+        after initialization by the owner of the object.
+
+        Parameters
+        ----------
+        kind : str
+            The type of reference (BibTex conventions).
+        key : str
+            The BibTex key of the reference
+        """
         self.kind = kind
         self.key = key
         self.tags = {}
 
     def get_url(self):
+        """Return a URL for this reference."""
         if 'doi' in self.tags:
             return 'http://dx.doi.org/%s' % self.tags['doi']
         elif 'url' in self.tags:
@@ -384,6 +611,7 @@ class Reference(object):
             return ''
 
     def format_text(self):
+        """Format and return a string representation suitable for printing on screen."""
         if self.kind == 'article':
             url = self.get_url()
             if len(url) > 0:
@@ -396,6 +624,7 @@ class Reference(object):
             raise NotImplementedError
 
     def format_rst(self):
+        """Format and return a string representation suitable for restructuredText."""
         if self.kind == 'article':
             url = self.get_url()
             if len(url) > 0:
@@ -410,7 +639,16 @@ class Reference(object):
 
 
 class Biblio(object):
+    """Collection of references, loaded from a BibTex file."""
+
     def __init__(self, filename):
+        """Initialize the Biblio object.
+
+        Parameters
+        ----------
+        filename : str
+            The BibTex file to load.
+        """
         self.filename = filename
         self._records = {}
         self._cited = {}
@@ -418,6 +656,7 @@ class Biblio(object):
         self._load(filename)
 
     def _load(self, filename):
+        """Load all reference from the given BibTex file."""
         with open(filename) as f:
             current = None
             for line in f:
@@ -445,12 +684,28 @@ class Biblio(object):
                     current.tags[tag] = value
 
     def cite(self, key, reason):
+        """Cite a reference from `data/references.bib` for given reason.
+
+        Parameters
+        ----------
+        key : str
+            The bibtex key in `data/references.bib`.
+        reason: str
+            The reason why this reference is cited, e.g. something in the form of
+            `"for using method bluh"`. You may want to cite the same reference for
+            different reasons from different parts of the code.
+
+        At the end of the program, a list of "relevant" references will be printed that
+        one should cite when using results obtained with a HORTON calculation.
+        """
         reasons = self._cited.setdefault(key, set([]))
         reasons.add(reason)
 
     def log(self):
+        """Report the cited references, typically at the end of a calculation."""
         if log.do_low:
-            log('When you use this computation for the preparation of a scientific pulication, cite the following references:')
+            log('When you use this computation for the preparation of a scientific',
+                'pulication, cite the following references:')
             log.hline()
             for key, reasons in sorted(self._cited.iteritems()):
                 log(self._records[key].format_text())
