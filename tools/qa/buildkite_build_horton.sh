c78616648124899ee34ff255d83c8d0727737c3b
Matthew Chan <chanmhy@mcmaster.ca>
2016-09-06 16:07:24 +0200

    moved unit_tests to same build

--- tools/qa/buildkite_build_horton.sh
+++ tools/qa/buildkite_build_horton.sh
@@ -15,6 +15,10 @@ echo "--- Build Cython files & HORTON"
 rm -rf installation
 ./setup.py install --prefix=`pwd`/installation
 
-echo "--- Uploading build artifact"
-tar -zcvf horton-install.tar.gz installation/
-#buildkite-agent artifact upload horton-install.tar.gz
+echo "--- Running Nosetests"
+cd installation
+PATH=$PATH:`pwd`/bin PYTHONPATH=`pwd`/lib/python2.7/site-packages:`pwd`/lib64/python2.7/site-packages HORTONDATA=`pwd`/share/horton nosetests -v --processes=2 --process-timeout=60 -a slow horton
+
+if [ "$BUILDKITE_PULL_REQUEST" = "false" ]; then
+  PATH=$PATH:`pwd`/bin PYTHONPATH=`pwd`/lib/python2.7/site-packages:`pwd`/lib64/python2.7/site-packages HORTONDATA=`pwd`/share/horton nosetests -v --processes=2 --process-timeout=60 -a "!slow" horton
+fi
