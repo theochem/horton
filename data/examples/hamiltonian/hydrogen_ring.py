cba70526a5dd4008a2c1c75e26e6f6fc9c9d3bd3
Toon Verstraelen <Toon.Verstraelen@UGent.be>
2016-09-20 09:49:55 +0200

    Minor cleanups

--- data/examples/hamiltonian/hydrogen_ring.py
+++ data/examples/hamiltonian/hydrogen_ring.py
@@ -5,20 +5,20 @@ import numpy as np
 from horton import *  # pylint: disable=wildcard-import,unused-wildcard-import
 
 
-# define the ring
+# Define the ring.
 natom = 11
-spacing = 1.3 # distance between two neighboring atoms in bohr
+spacing = 1.3  # distance between two neighboring atoms in bohr
 radius = spacing/(2*np.sin(np.pi/natom))
 
-# define the coordinates and elements
+# Define the coordinates and elements.
 coordinates = np.zeros((natom, 3))
-numbers = np.ones(natom, dtype=int) # must be integers
+numbers = np.ones(natom, dtype=int)  # must be integers
 for iatom in xrange(natom):
     angle = (2*np.pi/natom)*iatom
     coordinates[iatom, 0] = radius*np.cos(angle)
     coordinates[iatom, 1] = radius*np.sin(angle)
 
-# write the molecule to an XYZ file (optional)
+# Write the molecule to an XYZ file (optional).
 mol = IOData(coordinates=coordinates, numbers=numbers, title='H Ring')
 mol.to_file('ring.xyz')
 
