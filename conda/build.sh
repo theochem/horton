764dd2fa958aee0dcee30b19ed0520d65d83b45a
Matt Chan <matthew.chan@microsoft.com>
2024-03-09 13:28:09 -0800

    downgrade to libint < 2.8, stop using setup.py

--- conda/build.sh
+++ conda/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 
-./setup.py install --single-version-externally-managed --record=record.txt
-#python -m pip install . -vv
+#./setup.py install --single-version-externally-managed --record=record.txt
+python -m pip install . -vv
